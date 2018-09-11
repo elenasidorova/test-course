d648258b206bb95c9110daa56b779a8db3a7ef2f
elenasidorova <43114636+elenasidorova@users.noreply.github.com>
2018-09-11 23:30:58 +0300

    distance function added

---
+++ sandbox/src/main/java/pack/first/Point.java
@@ -0,0 +1,21 @@
+package pack.first;
+
+public class Point {
+    public double x;
+    public double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static double distance(Point a, Point b){
+        return Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2));
+    }
+
+    public static void main(String[] args){
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point (3,3);
+        System.out.println(distance(p1, p2));
+    }
+}
