3064e4a488fd65c3b7e5e111b0fe63b8f4f3114f
elenasidorova <43114636+elenasidorova@users.noreply.github.com>
2018-09-12 11:20:48 +0300

    method distanceTo added

--- sandbox/src/main/java/pack/first/Point.java
+++ sandbox/src/main/java/pack/first/Point.java
@@ -13,9 +13,14 @@ public class Point {
         return Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2));
     }
 
+    public double distanceTo(Point a){
+        return Math.sqrt(Math.pow(a.x - this.x, 2) + Math.pow(a.y - this.y, 2));
+    }
+
     public static void main(String[] args){
         Point p1 = new Point(1, 1);
-        Point p2 = new Point (3,3);
+        Point p2 = new Point (1,4);
         System.out.println(distance(p1, p2));
+        System.out.println(p1.distanceTo(p2));
     }
 }
