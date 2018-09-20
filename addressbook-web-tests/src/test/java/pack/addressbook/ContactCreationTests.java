c0e1dfd0f6b8c574a465d795f0999a5f13586372
elenasidorova <43114636+elenasidorova@users.noreply.github.com>
2018-09-21 02:53:34 +0300

    ApplicationManager created

--- addressbook-web-tests/src/test/java/pack/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/pack/addressbook/ContactCreationTests.java
@@ -6,10 +6,10 @@ public class ContactCreationTests extends TestBase {
 
     @Test
     public void testContactCreation() throws Exception {
-        initContactCreation();
-        fillContactForm(new ContactData("Mickey", "Mouse", "France, Disneyland Paris, b.1", "+33 000 000 00 0", "mickey123@test.com"));
-        submitContactCreation();
-        returnToHomePage();
+        app.initContactCreation();
+        app.fillContactForm(new ContactData("Mickey", "Mouse", "France, Disneyland Paris, b.1", "+33 000 000 00 0", "mickey123@test.com"));
+        app.submitContactCreation();
+        app.returnToHomePage();
     }
 
 }
