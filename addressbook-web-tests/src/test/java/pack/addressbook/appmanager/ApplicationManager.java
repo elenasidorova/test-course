68ef6ced8c91fe6fef66638dd51c26d2aa15eaba
elenasidorova <43114636+elenasidorova@users.noreply.github.com>
2018-09-21 04:46:01 +0300

    SessionHelper added

--- addressbook-web-tests/src/test/java/pack/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/pack/addressbook/appmanager/ApplicationManager.java
@@ -1,17 +1,14 @@
 package pack.addressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import pack.addressbook.model.ContactData;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
     WebDriver wd;
 
+    private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private ContactHelper contactHelper;
     private GroupHelper groupHelper;
@@ -23,25 +20,13 @@ public class ApplicationManager {
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         contactHelper = new ContactHelper(wd);
-        login("admin", "secret");
+        sessionHelper = new SessionHelper(wd);
+        sessionHelper.login("admin", "secret");
     }
 
     public void stop() {
-        logout();
+        sessionHelper.logout();
         wd.quit();
-    }
-
-    private void login(String username, String password) {
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(username);
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.xpath("//input[@value='Login']")).click();
-    }
-
-    private void logout() {
-        wd.findElement(By.linkText("Logout")).click();
     }
 
     public GroupHelper getGroupHelper() {
@@ -54,5 +39,9 @@ public class ApplicationManager {
 
     public NavigationHelper getNavigationHelper() {
         return navigationHelper;
+    }
+
+    public SessionHelper getSessionHelper() {
+        return sessionHelper;
     }
 }
